68a385737d6546ec9edc403dc8412684b8d7de08
Silvris <58583688+Silvris@users.noreply.github.com>
2023-06-17 15:12:08 -0500

    upload files

---
+++ scripts/autotracking/item_mapping.lua
@@ -0,0 +1,20 @@
+-- use this file to map the AP item ids to your items
+-- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
+-- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
+ITEM_MAPPING = {
+    [0x770001] = {"Burning", "toggle"},
+    [0x770002] = {"Burning", "toggle"},
+    [0x770003] = {"Burning", "toggle"},
+    [0x770004] = {"Burning", "toggle"},
+    [0x770005] = {"Burning", "toggle"},
+    [0x770006] = {"Burning", "toggle"},
+    [0x770007] = {"Burning", "toggle"},
+    [0x770008] = {"Burning", "toggle"},
+    [0x770010] = {"Burning", "toggle"},
+    [0x770011] = {"Burning", "toggle"},
+    [0x770012] = {"Burning", "toggle"},
+    [0x770013] = {"Burning", "toggle"},
+    [0x770014] = {"Burning", "toggle"},
+    [0x770015] = {"Burning", "toggle"},
+    [0x770020] = {"Burning", "consumable"},
+}
