f379bbd1a263ee31d344f027b0a3bf31b5c7f00c
Silvris <58583688+Silvris@users.noreply.github.com>
2024-06-05 21:30:17 -0500

    snes autotracking (untested?)

--- scripts/autotracking/snes.lua
+++ scripts/autotracking/snes.lua
@@ -1,24 +1,33 @@
 -- this is an example file for SNES memory autotracking 
 -- more info: https://github.com/black-sliver/PopTracker/blob/master/doc/AUTOTRACKING.md#memory-interface-usb2snes
-EXAMPLE_ADDR = 0x7E07F7
-EXAMPLE_SIZE = 0x8
+BASEADDR = 0xE00000
+U16 = 2
+U8 = 1
 
-function update_example(segment)
-    local readResult = segment:ReadUInt8(EXAMPLE_ADDR) -- prefered way of reading
-    local readResult2 = AutoTracker:ReadU16(EXAMPLE_ADDR + 0x1) -- alternative way of reading  
+HALKEN = BASEADDR + 0x80F0
+UNLOCKED_WORLD = BASEADDR + 0x53CB
+LEVELS = BASEADDR + 0x9020
+CURRENT_SAVE = BASEADDR + 0x3617
+
+worlds = []
+
+function update_available_worlds(segment)
+    local halken = segment:ReadUInt32(HALKEN)
+    if halken ~= 0x1802264936 then
+        return
+    end
+    -- this only checks "halk", but it's better than nothing?
+    for i=0,4 do
+        for j = 0,6 do
+            worlds[i,j] = segment:ReadUInt16(LEVELS+ (i*7)+j)
+        end
+    end
+    local unlocked_world = segment:ReadUInt16(UNLOCKED_WORLD)
+    local unlocked_level = segment:ReadUInt16(UNLOCKED_WORLD + 2)
+    
     if AUTOTRACKER_ENABLE_DEBUG_LOGGING_SNES then
-        print(string.format("update_example: readResult: %x, readResult2: %x", readResult, readResult2))
-    end
-    -- changing example item
-    local obj = Tracker:FindObjectForCode("toggle")
-    if obj then 
-        obj.Active = readResult2 > readResult
-    end
-    -- changing example section
-    local loc = Tracker:FindObjectForCode("@Example Parent/Example Location 1/Example Section 1")
-    if loc and readResult2 > readResult then
-        loc.AvailableChestCount = loc.AvailableChestCount - 1        
+        print(string.format("update_example: readResult: %x, readResult2: %x", unlocked_world, unlocked_level))
     end
 end
 
-ScriptHost:AddMemoryWatch('example', EXAMPLE_ADDR, EXAMPLE_SIZE, update_example)
+ScriptHost:AddMemoryWatch('WorldUnlock', UNLOCKED_WORLD, U16 * 2, update_available_worlds)
